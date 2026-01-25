39981528b3f64841484bb31aa558e2e4b7bb1684
MicATman <194826136+MicATman@users.noreply.github.com>
2026-01-25 12:39:05 +0100

    Update maus.py

--- scripts/maus/maus.py
+++ scripts/maus/maus.py
@@ -1,1 +1,23 @@
+import uinput
+from evdev import InputDevice, categorize, ecodes
 
+
+# Maus-Device öffnen (prüfe vorher mit: ls /dev/input/by-id/)
+# device_path = '/dev/input/by-id/usb-Logitech_USB_Optical_Mouse-event-mouse'
+device_path = '/dev/input/by-id/usb-Microsoft_Microsoft_Basic_Optical_Mouse-event-mouse'   # anpassen!
+mouse = InputDevice(device_path)
+
+# Uinput-Gerät anlegen
+keyboard = uinput.Device([uinput.KEY_RIGHT,uinput.KEY_LEFT])
+
+# print("Starte – bewege das Mausrad...")
+
+for event in mouse.read_loop():
+    if event.type == ecodes.EV_REL:   # relative Bewegung (z. B. Rad)
+        if event.code == ecodes.REL_WHEEL:
+            if event.value > 0:   # Rad nach oben
+                keyboard.emit_click(uinput.KEY_LEFT)
+#                 print("→ gedrückt")
+            elif event.value < 0: # Rad nach unten
+                keyboard.emit_click(uinput.KEY_RIGHT)
+#                 print("← gedrückt")
