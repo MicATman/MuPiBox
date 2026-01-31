19e7b33e81366f326487bce0cac589f8ecc74969
MicATman <194826136+MicATman@users.noreply.github.com>
2026-01-31 10:23:03 +0100

    Umschaltung Spotify / Mupibox
    Added button functionality to run a script when pressed.

--- scripts/maus/maus.py
+++ scripts/maus/maus.py
@@ -1,6 +1,13 @@
 import uinput
 from evdev import InputDevice, categorize, ecodes
+from gpiozero import Button
+from subprocess import call
+from signal import pause
 
+btn = Button(25, pull_up=True)
+def run_script():
+    call(["/usr/local/bin/mupibox/button.sh"])
+btn.when_pressed = run_script
 
 # Maus-Device öffnen (prüfe vorher mit: ls /dev/input/by-id/)
 # device_path = '/dev/input/by-id/usb-Logitech_USB_Optical_Mouse-event-mouse'
@@ -10,14 +17,11 @@ mouse = InputDevice(device_path)
 # Uinput-Gerät anlegen
 keyboard = uinput.Device([uinput.KEY_RIGHT,uinput.KEY_LEFT])
 
-# print("Starte – bewege das Mausrad...")
-
 for event in mouse.read_loop():
     if event.type == ecodes.EV_REL:   # relative Bewegung (z. B. Rad)
         if event.code == ecodes.REL_WHEEL:
             if event.value > 0:   # Rad nach oben
                 keyboard.emit_click(uinput.KEY_LEFT)
-#                 print("→ gedrückt")
             elif event.value < 0: # Rad nach unten
                 keyboard.emit_click(uinput.KEY_RIGHT)
-#                 print("← gedrückt")
+pause()
