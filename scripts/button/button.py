b013df3f11b68d037ec0abcf1acabaf25bc0851c
MicATman <194826136+MicATman@users.noreply.github.com>
2026-02-07 09:52:43 +0100

    Add button script to handle GPIO button press

--- scripts/button/button.py
+++ scripts/button/button.py
@@ -1,1 +1,11 @@
+from gpiozero import Button
+from subprocess import call
+from signal import pause
 
+btn = Button(25, pull_up=True, bounce_time=0.1)
+
+def run_script():
+    call(["/usr/local/bin/mupibox/button.sh"])
+
+btn.when_released = run_script
+pause()
